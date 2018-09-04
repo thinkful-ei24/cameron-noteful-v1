c47a24cf761d5aebb600ff150ccce5582db9ff79
Cameron Hatch <cameronlavigne92@gmail.com>
2018-09-04 13:45:01 -0500

    added get endpoint for note detail

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get('/api/notes', (req, res) => {
   res.json(data);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+  const item = data.find(i => i.id === Number(req.params.id));
+  res.json(item);
+});
+
 app.listen(8080, function(){
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
