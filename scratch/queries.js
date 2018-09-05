f057de6f9879877b91afaf3a086f229f1e488ff1
Cameron Hatch <cameronlavigne92@gmail.com>
2018-09-05 14:06:20 -0500

    added scrath folder to look at database

---
+++ scratch/queries.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const data = require('../db/notes');
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
+
+// GET Notes with search
+notes.filter('dogs', (err, list) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(list);
+});
+
+// GET Notes by ID
+notes.find(1005, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
+
+// PUT (Update) Notes by ID
+const updateObj = {
+  title: 'New Title',
+  content: 'Blah blah blah'
+};
+
+notes.update(1005, updateObj, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
+
+// CREATE Notes
+notes.create({title: 'New Note', content: 'dumb notes'}, (err,item)=>{
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
+
+// Delete Notes
+notes.delete(1001, (err,item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
